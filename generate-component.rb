211564cd60bec7e5697e7ea3360e63c06911611b
Travis Poulsen <travis.poulsen@gmail.com>
2017-08-31 10:26:46 -0400

    Release v0.4.4 of generate-component

--- generate-component.rb
+++ generate-component.rb
@@ -1,8 +1,8 @@
 class GenerateComponent < Formula
   desc "A component generator for React and React Native"
   homepage "https://github.com/tpoulsen/generate-component"
-  url "https://github.com/tpoulsen/generate-component/releases/download/v0.4.3/generate-component-0_4_3.tar.gz"
-  sha256 "99fe8010b50f44569fb6af33efb588256fbf0ac25fa2bdb21b7cd8c635d11c1f"
+  url "https://github.com/tpoulsen/generate-component/releases/download/v0.4.4/generate-component_0_4_4.tar.gz"
+  sha256 "39ebbfd9cb838d54bd218f0faf93b1e7b95f14aa649ceb55e35400c9ef8e1b2b"
 
   bottle :unneeded
 
